edf5826b8ca0f96411463a82f102607c3882afe4
claudio <cdjohnnatha@gmail.com>
2017-08-09 01:51:00 +0200

    hidded register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,30 +2,30 @@
 
 @section('content')
 
-  <section class="container col-sm-offset-2 col-sm-8">
-    <div class="panel panel-default">
-      <div class="panel-heading">Register</div>
-        <form class="form-horizontal" method="POST" action="{{ route('register') }}">
-          {{ csrf_field() }}
-          <section class="panel-body">
-                <section id="userInfo">
-                  @include('user._form')
-                </section>
-                <section id="addressSection">
-                  @include('address._form')
-                </section>
-                <section>
-                  <input type="hidden" name="subscription_id" value="1">
-                </section>
-          </section>
-          <footer class="panel-footer ">
-            <button type="submit" class="btn btn-info pull-right">Send</button>
-            <div class="clearfix"></div>
-          </footer>
+  {{--<section class="container col-sm-offset-2 col-sm-8">--}}
+    {{--<div class="panel panel-default">--}}
+      {{--<div class="panel-heading">Register</div>--}}
+        {{--<form class="form-horizontal" method="POST" action="{{ route('register') }}">--}}
+          {{--{{ csrf_field() }}--}}
+          {{--<section class="panel-body">--}}
+                {{--<section id="userInfo">--}}
+                  {{--@include('user._form')--}}
+                {{--</section>--}}
+                {{--<section id="addressSection">--}}
+                  {{--@include('address._form')--}}
+                {{--</section>--}}
+                {{--<section>--}}
+                  {{--<input type="hidden" name="subscription_id" value="1">--}}
+                {{--</section>--}}
+          {{--</section>--}}
+          {{--<footer class="panel-footer ">--}}
+            {{--<button type="submit" class="btn btn-info pull-right">Send</button>--}}
+            {{--<div class="clearfix"></div>--}}
+          {{--</footer>--}}
 
-        </form>
+        {{--</form>--}}
           {{--<register-form-button></register-form-button>--}}
-        </div>
-  </section>
+        {{--</div>--}}
+  {{--</section>--}}
 
 @endsection
