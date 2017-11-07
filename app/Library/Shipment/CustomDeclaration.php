557105959c3cb327e02856a9324c3229332640e1
claudio <cdjohnnatha@gmail.com>
2017-11-07 17:17:47 -0300

    created class for customdeclaration which will be use at goshippo

--- app/Library/Shipment/CustomDeclaration.php
+++ app/Library/Shipment/CustomDeclaration.php
@@ -11,5 +11,28 @@ namespace App\Library\Shipment;
 
 class CustomDeclaration
 {
+    private $custom_declarance;
 
+    function __construct($goods, $items)
+    {
+        $this->custom_declaration = array(
+            "contents_type" => $goods->content_type ? "MERCHANDISE" : 'GIFT',
+            "contents_explanation" => $goods->description,
+            "non_delivery_option" => "RETURN",
+            "certify" => true,
+            "certify_signer" => "Holyship",
+            "items" => $items->getItems(),
+            "metadata" => "Custom declarations"
+        );
+    }
+
+    function getCustomDeclaration()
+    {
+        return $this->custom_declaration;
+    }
+
+    function setCustomDeclaration($custom_declaration)
+    {
+        $this->custom_declaration = $custom_declaration;
+    }
 }
