41f68331124c4e4ebf355793cb92081113a2bdd0
claudio <cdjohnnatha@gmail.com>
2017-11-10 17:49:32 -0300

    created interface for repositories

---
+++ app/Repositories/Interfaces/RepositoryInterface.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: claudio
+ * Date: 11/10/17
+ * Time: 5:33 PM
+ */
+
+namespace App\Repositories\Interfaces;
+
+interface RepositoryInterface
+{
+    public function getAll();
+    public function store(array $attributes);
+    public function update($id, array $attributes);
+    public function find($column, $attribute);
+    public function destroy($id);
+
+}
